8057dc5ce583fe11f0f1a4694d5fcafa9c6a955e
rtokkie <natto.and.eggs@gmail.com>
2022-05-16 23:53:17 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,87 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormControlProps,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputProps,
+  Stack,
+  Textarea,
+  TextareaProps,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { Form, useField } from "react-final-form";
+
+type AppFormControlProps = { name: string } & FormControlProps;
+
+const AppFormControl: FC<AppFormControlProps> = ({ name, ...rest }) => {
+  const { meta } = useField(name, { subscription: { touched: true, error: true } });
+  return <FormControl {...rest} isInvalid={meta.touched && meta.error} />;
+};
+
+type AppFormErrorProps = { name: string };
+
+const AppFormError: FC<AppFormErrorProps> = ({ name }) => {
+  const { meta } = useField(name, { subscription: { error: true } });
+  return <FormErrorMessage>{meta.error}</FormErrorMessage>;
+};
+
+type AppInputControlProps = { name: string; label: string } & InputProps;
+
+const AppInputControl: FC<AppInputControlProps> = ({ name, label, ...rest }) => {
+  const { input, meta } = useField(name);
+  return (
+    <AppFormControl name={name} isRequired={rest.isRequired}>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <Input {...input} id={name} isInvalid={meta.touched && meta.error} />
+      <AppFormError name={name} />
+    </AppFormControl>
+  );
+};
+
+type AppTextAreaControlProps = { name: string; label: string } & TextareaProps;
+
+const AppTextAreaControl: FC<AppTextAreaControlProps> = ({ name, label, ...rest }) => {
+  const { input, meta } = useField(name);
+  return (
+    <AppFormControl name={name} isRequired={rest.isRequired}>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <Textarea {...input} id={name} isInvalid={meta.touched && meta.error} />
+      <AppFormError name={name} />
+    </AppFormControl>
+  );
+};
+
+const onSubmit = (v: any) => {
+  console.log(v);
+};
+
+const AppForm = () => {
+  return (
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <Box as="form" onSubmit={handleSubmit}>
+          <Stack>
+            <AppInputControl name="firstName" label="名前" />
+            <AppTextAreaControl name="otherComments" label="その他" />
+            <Button type="submit" colorScheme="green">
+              SAVE
+            </Button>
+          </Stack>
+        </Box>
+      )}
+    />
+  );
+};
 
 export const App = () => {
-  return <Container py="4">This is App</Container>;
+  return (
+    <Container py="4">
+      <AppForm />
+    </Container>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,30 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 
+const theme = extendTheme({
+  components: {
+    FormLabel: {
+      baseStyle: {
+        fontWeight: "semibold",
+        fontSize: "sm",
+      },
+    },
+    FormErrorMessage: {
+      baseStyle: {
+        fontSize: "sm",
+      },
+    },
+  },
+});
+
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("rootElement is not defined");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
